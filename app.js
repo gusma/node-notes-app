0077ff1500fecd27fb92d3984b58197a920fe2b2
gusma <gustavo.malamud@globallogic.com>
2020-08-05 13:24:42 -0300

    Progress...

---
+++ app.js
@@ -0,0 +1,7 @@
+const getNotes = require("./notes.js");
+const fs = require("fs");
+
+console.log(getNotes);
+
+// fs.writeFileSync("notes.txt", "Rancho Cucamonga");
+// fs.appendFileSync("notes.txt", "\nAdd a new line");
