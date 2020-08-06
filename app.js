fb9b0e081a83a9eb625a58c7606ef51eb926f57f
gusma <gustavo.malamud@globallogic.com>
2020-08-06 03:49:16 -0300

    Add add and remove functions

--- app.js
+++ app.js
@@ -1,7 +1,62 @@
-const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 
-// customize yargs version
+// Yargs version
 yargs.version("1.1.0");
-console.log(yargs.argv);
+
+// Add command
+yargs.command({
+	command: "add",
+	describe: "Add a new note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: "string",
+		},
+		body: {
+			describe: "Note body",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
+		notes.addNote(argv.title, argv.body);
+	},
+});
+
+// Remove command
+yargs.command({
+	command: "remove",
+	describe: "Remove a note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
+		notes.removeNote(argv.title);
+	},
+});
+
+// List command
+yargs.command({
+	command: "list",
+	describe: "List your notes",
+	handler: function () {
+		console.log("Listing out all notes");
+	},
+});
+
+// Read command
+yargs.command({
+	command: "read",
+	describe: "Read a note",
+	handler: function () {
+		console.log("Reading a note");
+	},
+});
+
+yargs.parse();
